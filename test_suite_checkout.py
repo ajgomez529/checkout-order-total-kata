51654843a7015fb034efef91c51aea70104c727c
Andrew Gomez <ajgomez529@gmail.com>
2021-03-21 00:05:53 -0500

    Added 3 passing test cases for item set up
    - Remove item successfully
    - Error when removing nonexistant item
    - Update item price successfully

--- test_suite_checkout.py
+++ test_suite_checkout.py
@@ -20,6 +20,26 @@ class ItemSetUp(unittest.TestCase):
         self.assertEqual(item.name, 'soup')
         self.assertEqual(item.price, 1.50)
         self.assertEqual(item.soldBy, 'unit')
+    
+    # add and remove item
+    def test_remove_item(self):
+        self.co_sys.register_item('soup', 1.50)
+        self.co_sys.unregister_item('soup')
+        self.assertEqual(self.co_sys.items.get('soup'), None)
+    
+    # attempt to remove nonexistant item produces KeyError
+    def test_remove_nonexistant_item(self):
+        self.assertRaises(KeyError, self.co_sys.unregister_item, 'ham')
+
+    # set price then change price
+    def test_price_update(self):
+        self.co_sys.register_item('bread', 1.25)
+        item = self.co_sys.items['bread']
+        self.assertEqual(item.price, 1.25)
+        self.co_sys.update_price('bread', 2.00)
+        self.assertEqual(item.price, 2.00)
+    
+
 
 if __name__ == '__main__':
     unittest.main()
