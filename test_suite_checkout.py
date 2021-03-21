ab042bca6a137b138456b0db53c9cf8814a9d59f
Andrew Gomez <ajgomez529@gmail.com>
2021-03-20 23:37:56 -0500

    Added two passing test cases
    - Properly store item sold by weight
    - Properly store item sold by unit

--- test_suite_checkout.py
+++ test_suite_checkout.py
@@ -1,4 +1,25 @@
 import unittest
+import checkout
+
+class ItemSetUp(unittest.TestCase):
+    def setUp(self):
+        self.co_sys = checkout.CheckoutSystem()
+    
+    # add weighted item and verify values
+    def test_add_weighed_item(self):
+        self.co_sys.register_item('steak', 7.99, 'lbs')
+        item = self.co_sys.items['steak']
+        self.assertEqual(item.name, 'steak')
+        self.assertEqual(item.price, 7.99)
+        self.assertEqual(item.soldBy, 'lbs')
+
+    # add unit item and verify values
+    def test_add_unit_item(self):
+        self.co_sys.register_item('soup', 1.50)
+        item = self.co_sys.items['soup']
+        self.assertEqual(item.name, 'soup')
+        self.assertEqual(item.price, 1.50)
+        self.assertEqual(item.soldBy, 'unit')
 
 if __name__ == '__main__':
     unittest.main()
